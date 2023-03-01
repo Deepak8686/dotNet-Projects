6753ff83cff32a3200cba15fed100c90eefc0246
Meghashri123 <126661194+Meghashri123@users.noreply.github.com>
2023-03-01 16:56:19 +0530

    file added

---
+++ Megashree S/ECBill/ECBill/Calculation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECBill
+{
+     public class Calculation : Switchcase
+    {
+        public void Bill(int units)
+        {
+            var charge= units <= 100 ? units * 0 :
+            units <= 1000 ? (units - 100) * 5 :
+            units <= 10000 ? (900 * 5) + (units - 1000) * 10 :
+            units <= 30000 ? (900 * 5) + (9000 * 10) + (units - 10000) * 20 :
+            (900 * 5) + (9000 * 10) + (20000 * 20) + (units - 30000) * 35;
+            switchA(charge);
+        }
+    }
+}
---
+++ Megashree S/ECBill/ECBill/Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Data.SqlClient;
+namespace ECBill
+
+{
+    class EBbill : Calculation, Iebill
+    {
+
+        public void UserInputs()
+        {
+            while (true)
+            {
+                Console.WriteLine("--------Electricity Bill---------\n");
+
+                int billID = GetIntFromUser("Enter bill id");
+                int units = GetIntFromUser("Enter units");
+                Bill(units);
+            }
+
+        }
+        public int GetIntFromUser(string message)
+        {
+            int number = 0;
+            while (number == 0)
+            {
+                try
+                {
+                    Console.WriteLine(message);
+                    number = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid number.");
+                }
+            }
+            return number;
+        }
+    }
+}
+
+
