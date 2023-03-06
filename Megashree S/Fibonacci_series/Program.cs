f23621a4c1a6a3f92ad0652e08448c0f3ae72fdf
Meghashri123 <126661194+Meghashri123@users.noreply.github.com>
2023-03-06 15:01:00 +0530

    Fibonacci_Series

---
+++ Megashree S/Fibonacci_series/Program.cs
@@ -0,0 +1,15 @@
+﻿using Fibonacci_series;
+using System;
+
+public class Program
+{
+        static void Main(string[] args)
+        {
+        InputFib userInput = new InputFib();
+        Fibmethod calculator = new Fibmethod(userInput); 
+        calculator.CalculateFibonacciSeries();
+        Console.ReadKey();
+        }
+    }
+
+
