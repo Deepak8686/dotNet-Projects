60e79d437accd7e4a2017accf5a58e777f217be0
Supritha603 <supritha.s@joyitsolutions.co>
2023-03-06 14:51:06 +0530

    Recursive Fibonacci Series using Abstract Factory Pattern

---
+++ Supritha S/Recursive Fibonacci/Recursive Fibonacci/RecursiveFibonacciFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursive_Fibonacci
+{
+    public class RecursiveFibonacciFactory : IFibonacciFactory
+    {
+        public IFibonacci Create()
+        {
+            return new RecursiveFibonacci();
+           
+        }
+    }
+}
