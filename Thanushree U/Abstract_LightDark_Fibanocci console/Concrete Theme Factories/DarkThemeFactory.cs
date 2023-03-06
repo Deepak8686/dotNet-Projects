04eee4bdde4c98d4b3c2416c65b229950cfc7e33
thanu1123 <126650082+thanu1123@users.noreply.github.com>
2023-03-07 00:13:44 +0530

    AFP for L/D theme and generate Fibonacci series

---
+++ Thanushree U/Abstract_LightDark_Fibanocci console/Concrete Theme Factories/DarkThemeFactory.cs
@@ -0,0 +1,30 @@
+﻿using Abstract_LightDark_Themes.Concrete_Theme_properties;
+using Abstract_LightDark_Themes.FactoryItems;
+using Abstract_LightDark_Themes.Theme_Factory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_LightDark_Themes.Concrete_Theme_Factories
+{
+    public class DarkThemeFactory:IThemeFactory
+    {
+        public IButton CreateButton()
+        {
+            return new DarkButton();
+        }
+
+        public ILabel CreateLabel()
+        {
+            return new DarkLabel();
+        }
+
+        public ITextBox CreateTextBox()
+        {
+            return new DarkTextBox();
+        }
+    }
+}
---
+++ Thanushree U/Abstract_LightDark_Fibanocci console/Program.cs
@@ -0,0 +1,23 @@
+﻿using Abstract_LightDark_Themes.Concrete_Theme_Factories;
+using Abstract_LightDark_Themes.FactoryItems;
+using Abstract_LightDark_Themes.Theme_Factory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Abstract_LightDark_Themes.final;
+
+namespace Abstract_LightDark_Themes
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            final theme = new final();
+            theme.Choice();
+
+            Console.ReadKey();
+        }
+    }
+}
