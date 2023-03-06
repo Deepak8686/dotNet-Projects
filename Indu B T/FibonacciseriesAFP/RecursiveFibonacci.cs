59032106d2ebb061b9554bbde592d09c8a5c30bf
Indu120699 <indumurthy120699@gmail.com>
2023-03-06 15:53:26 +0530

    Fibonacci series

---
+++ Indu B T/FibonacciseriesAFP/RecursiveFibonacci.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonacciseriesAFP
+{
+    public class RecursiveFibonacci : IFibonacci
+    {
+        public int Calculate(int n)
+        {
+            if (n <= 1)
+            {
+                return n;
+            }
+            else
+            {
+                return Calculate(n - 1) + Calculate(n - 2);
+            }
+        }
+    }
+}
