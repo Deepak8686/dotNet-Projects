ea839987e4c20a02f1de6742c3b0c71e501ccb1c
SubhashR8 <126658900+SubhashR8@users.noreply.github.com>
2023-03-09 18:02:30 +0530

    Itterative and Recurcive Approach Using AbstractFactory Pattern

---
+++ Subhash R/Itterativeapproach/MainFactory/ConcreateIterativeFactory/ITCalculation/Iterativefibonacci.cs
@@ -0,0 +1,42 @@
+﻿using Itterativeapproach.MainFactory.IterativeFactory.InterfaceIterative;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Itterativeapproach.MainFactory.IterativeFactory.ITCalculation
+{
+    public class Iterativefibonacci : ITerativeInterface1
+    {
+        private readonly int _IFibo;
+
+        public Iterativefibonacci(int IFibo)
+        {
+            _IFibo = IFibo;
+        }
+        public void Iterativefibo()
+        {
+
+            try
+            {
+                int firstnumber = 0, secondnumber = 1, result, i;
+                Console.WriteLine(" the length of fibonaci series");
+                Console.Write(firstnumber+" "+secondnumber+" ");
+                for (i=2; i < _IFibo; i++)
+                {
+                    result=firstnumber + secondnumber;
+                    Console.Write(result+"  ");
+                    firstnumber=secondnumber;
+                    secondnumber=result;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("enter a numercal value");
+            }
+
+        }
+    }
+}
---
+++ Subhash R/Itterativeapproach/MainFactory/ConcreateRecursiveFactory/InterfaceRecursive/RecursiveInterface1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Itterativeapproach.MainFactory.RecursiveFactory.InterfaceRecursive
+{
+    public interface RecursiveInterface1
+    {
+        void Recursivecal();
+    }
+}
