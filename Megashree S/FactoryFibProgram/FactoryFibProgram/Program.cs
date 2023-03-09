ff3b865d1c166cae4c292d4d7a2f9cb98dffd346
Meghashri123 <126661194+Meghashri123@users.noreply.github.com>
2023-03-09 17:35:57 +0530

    FactoryFibonacciSeries

---
+++ Megashree S/FactoryFibProgram/FactoryFibProgram/Program.cs
@@ -0,0 +1,27 @@
+﻿
+using FactoryFibProgram;
+using FactoryFibProgram.Classes;
+using FactoryFibProgram.Interface;
+using System;
+
+public class Program
+{
+    private static int userInput;
+
+    static void Main(string[] args)
+    {
+       
+        IterativeInterface calculator1 = new IterativeClass(userInput);
+        calculator1.Iterative();
+
+        RecursiveInterface calculator = new RecursiveClass(userInput);
+        calculator.CalculateFibonacciSeries();
+      
+        CallClass cal = new CallClass();
+        cal.userInputs();
+        Console.ReadKey();
+
+    }
+}
+
+
