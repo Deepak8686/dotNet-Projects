6fd0629a1c27d9517af9d0b493e8566ce9653c22
Shivakumara1999 <126660929+Shivakumara1999@users.noreply.github.com>
2023-03-09 15:46:28 +0530

    Fibonacci Iteration and Recursion

---
+++ Shivakumara/Factory_Fibonacci/Factory_Fibonacci/Factory/factory_iteration/Interface/Iteration_Interface.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factory_Fibonacci.Factory.factory_iteration.Interface
+{
+    public interface Iteration_Interface
+    {
+        public void Iteration();
+    }
+}
