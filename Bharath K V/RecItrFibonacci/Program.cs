36271adcb6fb59af8e975024031f9a09f9c43630
Bharathkvb <59308629+Bharath-kukkunoor@users.noreply.github.com>
2023-03-14 17:01:14 +0530

    Fibonacci Series

--- Bharath K V/RecItrFibonacci/Program.cs
+++ Bharath K V/RecItrFibonacci/Program.cs
@@ -1,72 +1,26 @@
-﻿using RecItrFibonacci.RecursiveInterface;
-using RecItrFibonacci.RecursiveIterativefib.IterativeFibonacci;
-using RecItrFibonacci.RecursiveIterativefib.Recursive_Fibonacci;
+﻿using RecItrFibonacci.RecItrClass;
+using RecItrFibonacci.RecItrInputs;
+using RecItrFibonacci.RecItrInterface;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace Fibonacci
+
+namespace RecItrFibonacci
 {
-    //interface IFibonacciSeries
-    //{
-    //    void Generate();
-    //}
-
-    class FibonacciSeriesFactory
+    public class Program
     {
-        public static IFibonacciSeries Create(string type, int n)
-        {
-            switch (type.ToLower())
-            {
-                case "recursive":
-                    return new RecursiveFibonacciSeries(n);
-                case "iterative":
-                    return new IterativeFibonacciSeries(n);
-                default:
-                    throw new ArgumentException("Invalid type");
-            }
-        }
-    }
-
-    class Program
-    {
+        private static int Input;
         static void Main(string[] args)
-        {
-            Console.Write("Enter the maximum value of the Fibonacci numbers to generate: ");
-            int n;
-            while (!int.TryParse(Console.ReadLine(), out n) || n <= 0)
-            {
-                Console.WriteLine("Please enter a positive integer greater than 0");
-                Console.Write("Enter the maximum value of the Fibonacci numbers to generate: ");
-            }
-
-            Console.WriteLine("Select Fibonacci type:");
-            Console.WriteLine("1. Recursive");
-            Console.WriteLine("2. Iterative");
-            int selection;
-            while (!int.TryParse(Console.ReadLine(), out selection) || selection < 1 || selection > 2)
-            {
-                Console.WriteLine("Please enter a valid selection (1 or 2)");
-                Console.WriteLine("Select Fibonacci type:");
-                Console.WriteLine("1. Recursive");
-                Console.WriteLine("2. Iterative");
-            }
-
-            IFibonacciSeries series;
-            switch (selection)
-            {
-                case 1:
-                    series = FibonacciSeriesFactory.Create("recursive", n);
-                    break;
-                case 2:
-                    series = FibonacciSeriesFactory.Create("iterative", n);
-                    break;
-                default:
-                    Console.WriteLine("Invalid selection");
-                    return;
-            }
-
-            series.Generate();
-
-            Console.ReadKey(); // Keep console open until user hits enter
+        {       
+            RecursiveIterativeInterface RecItrCal = new RecursiveIterativeClass(Input);
+            RecItrCal.RecursiveFibonacci();
+            RecItrCal.IterativeFibonacci();
+            RecursiveIterativeInputs Calculate = new RecursiveIterativeInputs();
+            Calculate.Inputs(); 
+            Console.ReadKey();
         }
     }
 }
---
+++ Bharath K V/RecItrFibonacci/RecItrInputs/RecursiveIterativeInputs.cs
@@ -0,0 +1,88 @@
+﻿using RecItrFibonacci.RecItrClass;
+using RecItrFibonacci.RecItrInterface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecItrFibonacci.RecItrInputs
+{
+     public class RecursiveIterativeInputs
+    {
+        public void Inputs()
+        {
+            while (true)
+            {
+                try
+                {
+                    RecursiveIterativeInterface sel1;
+                    Console.WriteLine("\nEnter the type of series to be generate: \n 1.Recursive Fibonacci \n 2.Iterative Fibonacci");
+                    double selection = int.Parse(Console.ReadLine());
+                    if (selection != 1 && selection != 2)
+                    {
+                        Console.WriteLine("Invalid Option Selection.");
+                        continue;
+                    }
+                    double getin = GetInput("Enter the Number of Terms for Fibonacci series:");
+                    Console.WriteLine("Fibonacci Series: ");
+                    if (selection == 1)
+                    {
+                        sel1= new RecursiveIterativeClass(getin);
+                        sel1.RecursiveFibonacci();
+                    }
+                    else if (selection == 2)
+                    {
+                        sel1 = new RecursiveIterativeClass(getin);
+                        sel1.IterativeFibonacci();
+                    }
+                        while (true)
+                        {
+                            Console.WriteLine("\n\n Generate Series again?.Press 'y' to Continue,Press 'n' to Exit");
+                            char checkdata = Console.ReadKey().KeyChar;
+                            if (checkdata == 'y')
+                            {
+                                Inputs();
+                            }
+                            else if (checkdata == 'n')
+                            {
+                                Environment.Exit(0);
+                            }
+                            else
+                            {
+                                Console.WriteLine("\nInvalid input. Press 'y' to Generate Series again.Press 'n' to Exit.");
+                            }
+                        }
+                    }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Invalid Option! Please Enter a Number.");
+                }
+            }
+        }
+        private double GetInput(string message)
+        {
+            double num;
+            while (true)
+            {
+                Console.WriteLine(message);
+                try
+                {
+                    num = Convert.ToDouble(Console.ReadLine());
+                    if (num < 0)
+                    {
+                        Console.WriteLine("Invalid Option! Please Enter a Positive Number.");
+                    }
+                    else
+                    {
+                        return num;
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Invalid Option! Please Enter a Number.");
+                }
+            }
+        }
+    }
+}
