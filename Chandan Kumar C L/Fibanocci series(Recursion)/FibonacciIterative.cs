9c716a631d2cb8c457ebc5c368764c4e8cc511e9
Chandan0037 <108929093+Chandan0037@users.noreply.github.com>
2023-03-14 16:21:58 +0530

    Updated Fibonacci

---
+++ Chandan Kumar C L/Fibanocci series(Recursion)/FibonacciIterative.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibanocci_series_Recursion_
+{
+    public class FibonacciIterative : IFibonacciMethods
+    {
+
+        public int fib(double a)
+        {
+            //int sum = 0;
+            int n1 = 0;
+            int n2 = 1;
+            int sum = 0;
+            //Console.WriteLine(b);
+            //Console.Write(a + " " + b + " ");
+
+            if (a == 0)
+            {
+                sum = 0;
+            }
+            else if (a == 1)
+            {
+                Console.WriteLine("0\n");
+            }
+            else
+            {
+                Console.Write(n1 + "\n" + n2 + "\n");
+                for (int j = 2; j < a; j++)
+                {
+                    sum = n1 + n2;
+                    Console.WriteLine(sum);
+                    n1 = n2;
+                    n2 = sum;
+                } 
+            } 
+            return sum;
+        }
+    }
+}
---
+++ Chandan Kumar C L/Fibanocci series(Recursion)/IFibonacciMethods.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibanocci_series_Recursion_
+{
+    public interface IFibonacciMethods
+    {
+        int fib(double i);
+    }
+}
