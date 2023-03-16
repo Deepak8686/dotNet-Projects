a85f517f75595b8e6fa0cbc0bc143516f13bbe57
Meghashri123 <126661194+Meghashri123@users.noreply.github.com>
2023-03-16 16:51:08 +0530

    Client Server Socket Program

---
+++ Megashree S/ClientServerProgram/ClientPro/ClientPro/Program.cs
@@ -0,0 +1,19 @@
+﻿using ClientPro.Interface;
+using ClientPro.Repository;
+using System;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ClientPro
+{
+    public class Program
+    {
+        static void Main(string[] args)
+
+        {
+          IClient obj = new ClientRepo();
+            obj.ClientRepos();
+        }
+    }
+}
+
---
+++ Megashree S/ClientServerProgram/ClientPro/ClientPro/Repository/ClientRepo.cs
@@ -0,0 +1,38 @@
+﻿using ClientPro.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientPro.Repository
+{
+    public class ClientRepo : IClient
+    {
+        public void ClientRepos()
+        {
+            TcpClient client = new TcpClient("localhost", 8888);
+
+            Console.WriteLine("Client is connected with the server.");
+
+            NetworkStream stream = client.GetStream();
+
+            byte[] data = new byte[1024];
+            int bytes = stream.Read(data, 0, data.Length);
+            string message = Encoding.ASCII.GetString(data, 0, bytes);
+
+            Console.WriteLine("Server: " + message);
+
+            Console.Write("Enter message to send to server: ");
+            string input = Console.ReadLine();
+
+            data = Encoding.ASCII.GetBytes(input);
+            stream.Write(data, 0, data.Length);
+
+            client.Close();
+
+            Console.WriteLine("Connection closed.");
+        }
+    }
+}
