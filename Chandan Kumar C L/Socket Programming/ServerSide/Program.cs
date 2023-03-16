d31cbe2ad44cd016369f8d9f5133184a5f39e852
Chandan0037 <108929093+Chandan0037@users.noreply.github.com>
2023-03-16 16:37:26 +0530

    Socket Programming with pattern

---
+++ Chandan Kumar C L/Socket Programming/ServerSide/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using ServerSide.Interface;
+using ServerSide.Repository;
+
+namespace ServerSide
+{
+    public class Program  
+    {
+        static void Main(string[] args)
+        {
+            IServer server = new ServerRepo();
+            server.ServerSocket();
+        }
+    }
+}
