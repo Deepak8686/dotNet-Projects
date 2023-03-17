bf8aa08c6723e0729e166566853ce64dcb072d44
Bharathkvb <59308629+Bharath-kukkunoor@users.noreply.github.com>
2023-03-17 10:24:56 +0530

    Client Server Socket

---
+++ Bharath K V/Client Server Socket/Client Side/Client Side/Program.cs
@@ -0,0 +1,19 @@
+﻿using Client_Side.ClientAbstract;
+using Client_Side.ClientInterface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client_Side
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            IClientInterface client = new ClientSideClass();
+            client.StartClient();
+        }
+    }
+}
